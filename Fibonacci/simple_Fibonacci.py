a4a57fc757356e641c631023a6e16fd278c6d6fe
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 14:46:58 +0530

    Update simple_Fibonacci.py

--- Fibonacci/simple_Fibonacci.py
+++ Fibonacci/simple_Fibonacci.py
@@ -1,7 +1,7 @@
-n = int(input("Enter a number: "))
-a, b = 0, 1
-while a <= n:
-    print("The Fibonacci series for", n, "number is :", a)
+n = int(input("Enter a number: ")) # take input from user
+a, b = 0, 1 # assign values to a and b
+while a <= n: # loop
+    print(a) # print the series
     temp = a
     a = b
     b = temp + b
