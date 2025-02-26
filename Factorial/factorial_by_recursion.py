1c18a15ed4178420e7b0593b42cce6c1da8ec50b
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 14:41:29 +0530

    Update factorial_by_recursion.py

--- Factorial/factorial_by_recursion.py
+++ Factorial/factorial_by_recursion.py
@@ -1,8 +1,8 @@
 # factorial of a number using recursion
 
-def factorial(n):
+def factorial(n): #function defined
     if n == 0 or n == 1:
         return 1
-    return  n * factorial(n - 1)  
+    return  n * factorial(n - 1)   # function calling itself
 
-print(factorial(int(input("enter number :"))))
+print(factorial(int(input("enter number :")))) #taking input from user and printing the output
