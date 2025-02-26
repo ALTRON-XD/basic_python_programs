18a25c16ce0e72c273fa06316c58d592c7b6e284
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 10:43:20 +0530

    Create factorial_by_range.py

---
+++ Factorial/factorial_by_range.py
@@ -0,0 +1,8 @@
+# factorial of a number using recursion
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return  n * factorial(n - 1)  
+
+print(factorial(int(input("enter number :"))))
