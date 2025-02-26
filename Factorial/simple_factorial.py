0484cab976540f68541956cdbe8e8ca1526ddf76
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 14:43:26 +0530

    Update simple_factorial.py

--- Factorial/simple_factorial.py
+++ Factorial/simple_factorial.py
@@ -1,9 +1,9 @@
 # factorila of a number
 
-num = int(input("enter number :"))
-fact = 1
-while num >0:
-    fact=fact*num
+num = int(input("enter number :")) # input from user
+fact = 1 # initial value of fact
+while num >0: # loop to calculate factorial
+    fact=fact*num 
     num=num-1 
 
-print("factorial of number is :",fact)
+print("factorial of number is :",fact) 
