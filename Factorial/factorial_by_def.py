65471986133d2eaad4b88ca9e39834d9a7b5615e
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 14:38:22 +0530

    Update factorial_by_def.py

--- Factorial/factorial_by_def.py
+++ Factorial/factorial_by_def.py
@@ -1,8 +1,8 @@
-def factorial(a):
+def factorial(a): #function defined
     result = 1
-    for i in range(1, a + 1):
+    for i in range(1, a + 1): #loop  with range
         result *= i
     print(result)
     return result
 
-factorial(int(input("Enter a number: ")))
+factorial(int(input("Enter a number: "))) # function calling
