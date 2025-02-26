78b84f46ea252b0193474e1911df5769b0701ea4
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 15:01:44 +0530

    Update Fibonacci_by_def.py

--- Fibonacci/Fibonacci_by_def.py
+++ Fibonacci/Fibonacci_by_def.py
@@ -1,10 +1,10 @@
-def Fibonacci(a):
-    a, b = 0, 1
-    while a <= 0:
+def Fibonacci(n): # function definition
+    a, b = 0, 1 # multiple assignment
+    while a <= n: # while loop
         print(a)
         temp = a
         a = b
-        b = temp + b
-    return a
+        b = a + temp
+    return a # return statement
 
-Fibonacci(int(input("Enter a number: ")))
+Fibonacci(int(input("Enter a number: "))) # function call
