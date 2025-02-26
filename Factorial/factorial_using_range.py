663ac898abc69efe3b9ec012d9844aaa7ebda36c
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-02-26 14:42:20 +0530

    Update factorial_using_range.py

--- Factorial/factorial_using_range.py
+++ Factorial/factorial_using_range.py
@@ -1,7 +1,7 @@
 # factorial of a number using range function
 
-num = int(input("enter number :"))
-fact = 1
-for i in range(1, num + 1):
-    fact = fact * i
-print("factorial of number is :", fact)
+num = int(input("enter number :")) # input from user
+fact = 1 # initial value of fact
+for i in range(1, num + 1): # range function
+    fact = fact * i 
+print("factorial of number is :", fact) 
