6153ac97740fec414cb866c2f9981fe27a3a318e
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-03-05 13:34:29 +0530

    Update sum.py

--- list/sum.py
+++ list/sum.py
@@ -1,1 +1,4 @@
-#create a list of 5 num and print their sum
+#write a list and print its sum
+num = list(map(int, input("enter the numbers: ").split())) #taking input from user
+sm = sum(num) #sum of the list
+print(sm) #printing the sum of the list2
