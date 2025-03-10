7929d3ac77aea2dcbb6401f029e8db9c7874fa35
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-03-10 19:15:00 +0530

    Create merge.py

---
+++ dictionary/merge.py
@@ -0,0 +1,9 @@
+# Merge two dictionaries into one.
+dic1 = {
+    input("enter a key : ") : input("enter a value : ")
+}
+dic2 = {
+    input("enter a key : ") : input("enter a value : ")
+}
+dic1.update(dic2) #logic
+print(dic1)
